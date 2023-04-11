1b4cdb2916dae191bf910eab2943530d3e6cec7b
Viktor Ryabov <iprva@ya.ru>
2023-04-11 18:00:28 +0300

    added first riddle

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const answerFunc = () => {
+    let answer = prompt("Знаете ответ?", "answer");
+    let go = window.confirm("Поехали?");
+    if(answer == "проверка"){
+      alert ("Правильно, сначала нужно протестировать, а потом давать людям!");
+      alert ("Вот теперь начинается настоящий  квест");
+      if (go){
+        window.location.href = 'https://rya-travel.ru';
+      }
+    }
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="App-key">234123fd3fcwerv</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Аве, Цезарь!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          onClick={()=>(
+            answerFunc()
+
+          )}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          :>
         </a>
       </header>
     </div>
