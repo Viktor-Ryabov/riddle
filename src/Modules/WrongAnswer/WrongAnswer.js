af35af35b3fd01fffe85915b30869622eb654c91
Viktor Ryabov <iprva@ya.ru>
2023-04-19 11:52:49 +0300

    router worcks

--- src/Modules/WrongAnswer/WrongAnswer.js
+++ src/Modules/WrongAnswer/WrongAnswer.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Styles from "./WrongAnswer.module.css";
+import { useDispatch } from "react-redux";
 
 const WrongAnswer = () => {
+    const dispatch = useDispatch();
 
     return (
         <div className={Styles.container}>
             <div className={Styles.inputs}>
-                <div className={Styles.brand_title}>Ваш ответ не правильный</div>
+                <div className={Styles.brand_title}>
+                    Ваш ответ не правильный
+                </div>
 
                 <button
                     className={Styles.button}
                     type="send"
+                    onClick={() => {
+                        dispatch({ type: "WRONG_ANSWER_DISABLED" });
+                    }}
                 >
                     ок, подумаю ещё
                 </button>
