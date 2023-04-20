43ee865567f9d2c14890e8b36e2a09f853af8778
Viktor Ryabov <iprva@ya.ru>
2023-04-20 17:59:31 +0300

    structure was added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Styles from "./Header.module.css";
 import ActionButton from "../../UI/ActionButton/ActionButton";
+import { withModal } from "../../hoc/withModal";
+import EnteryForm from "../../Modules/EnteryForm/EnteryForm";
+import { useSelector } from "react-redux";
 
+const WithModalEntery = withModal(EnteryForm);
 const Header = () => {
+    const state = useSelector((state) => state);
+
     return (
         <div className={Styles.header}>
+            <WithModalEntery active={state.enteryPopupActive} />
             <ActionButton text1="Главная" to="/" />
             <ActionButton text1="Личный кабинет" to="/personal_page" />
             <ActionButton text1="Текущая загадка" to="/first" />
