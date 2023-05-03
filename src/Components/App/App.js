29b74ad0a2ad9f8d422bb05ab380086fe4489eb3
Viktor Ryabov <iprva@ya.ru>
2023-05-03 12:48:15 +0300

    added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Riddle1 from "../Riddle1/Riddle1.js";
 import Riddle2 from "../Riddle2/Riddle2.js";
 import Riddle3 from "../Riddle3/Riddle3.js";
@@ -7,7 +7,6 @@ import Riddle4 from "../Riddle4/Riddle4";
 import Riddle5 from "../Riddle5/Riddle5";
 import { Provider } from "react-redux";
 import { store } from "../../store/store.js";
-import Main from "../../Pages/Main/Main";
 import About from "../../Pages/About/About";
 import Contacts from "../../Pages/Contacts/Contacts";
 import PersonalPage from "../../Pages/PersonalPage/PersonalPage";
@@ -15,26 +14,34 @@ import Header from "../Header/Header";
 import Rules from "../../Pages/Rules/Rules";
 import Auth from "../Auth";
 
+let promice = () => {
+    fetch("https://official-joke-api.appspot.com/jokes/")
+    .then(response => response.json())
+    .then(response => console.log(response))
+
+};
 
 function App() {
+    // promice();
+
     return (
         <Provider store={store}>
-                <div className="App">
-                    <Header/>
-                    <Routes>
-                        <Route exact path="*" element={<Auth />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contacts" element={<Contacts />} />
-                        <Route path="/personal_page" element={<PersonalPage />} />
-                        <Route path="/rules" element={<Rules />} />
-                        <Route path="/first" element={<Riddle1 />} />
-                        <Route path="/second" element={<Riddle2 />} />
-                        <Route path="/third" element={<Riddle3 />} />
-                        <Route path="/fourth" element={<Riddle4 />} />
-                        <Route path="/fifth" element={<Riddle5 />} />
-                    </Routes>
-                    {/* <Footer /> */}
-                </div>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route exact path="*" element={<Auth />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                    <Route path="/personal_page" element={<PersonalPage />} />
+                    <Route path="/rules" element={<Rules />} />
+                    <Route path="/first" element={<Riddle1 />} />
+                    <Route path="/second" element={<Riddle2 />} />
+                    <Route path="/third" element={<Riddle3 />} />
+                    <Route path="/fourth" element={<Riddle4 />} />
+                    <Route path="/fifth" element={<Riddle5 />} />
+                </Routes>
+                {/* <Footer /> */}
+            </div>
         </Provider>
     );
 }
