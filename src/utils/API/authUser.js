ba6c7e99cf490fbe451d3df9fa2ccca2eb4076e5
Viktor Ryabov <iprva@ya.ru>
2023-05-11 17:17:13 +0300

    added auth

--- src/utils/API/authUser.js
+++ src/utils/API/authUser.js
@@ -1,22 +1,26 @@
 import { BASE_URL } from "../../constants/routeConstants";
 
-const authorizeUserRequestOptions = (data) => {
-    return {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: data
-    };
-};
 
 export const authorizeUser = (userEmail, userPassword) => {
-    const data = new URLSearchParams();
-    data.append("grant_type", "password");
-    data.append("username", `${userEmail}`);
-    data.append("password", `${userPassword}`);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", "Basic Y2xpZW50OnNlY3JldA==");
+    myHeaders.append("Cookie", "JSESSIONID=489CF0241F24DB93A9A93E2B60905121");
+
+    const raw = "";
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
     fetch(
-        `${BASE_URL}/oauth/token?`,
-        authorizeUserRequestOptions()
+        `${BASE_URL}/oauth/token?grant_type=password&username=${userEmail}&password=${userPassword}`,
+        requestOptions
     )
         .then((response) => response.json())
-        .then((response) => console.log(response));
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
 };
